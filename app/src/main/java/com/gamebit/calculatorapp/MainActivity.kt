6c975f5412c892ffe9944c4ba196025c37272041
bariskarapinar <baris.karapinar@tokeninc.com>
2022-06-19 15:32:33 +0300

    Coding the Calculator

--- app/src/main/java/com/gamebit/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/gamebit/calculatorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.gamebit.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -43,6 +44,21 @@ class MainActivity : AppCompatActivity() {
         val buttonMinus = findViewById<Button>(R.id.buttonMinus)
         val buttonPlus = findViewById<Button>(R.id.buttonPlus)
 
+        val listener = View.OnClickListener { v ->
+            val b = v as Button
+            newNumber.append(b.text)
+        }
+
+        button0.setOnClickListener(listener)
+        button1.setOnClickListener(listener)
+        button2.setOnClickListener(listener)
+        button3.setOnClickListener(listener)
+        button4.setOnClickListener(listener)
+        button5.setOnClickListener(listener)
+        button6.setOnClickListener(listener)
+        button7.setOnClickListener(listener)
+        button8.setOnClickListener(listener)
+        button9.setOnClickListener(listener)
 
     }
 
